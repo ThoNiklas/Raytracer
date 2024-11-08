29334606e836050301fc4b1d67030749aef65359
ThoNiklas <niklas.thomas.99@gmx.de>
2024-11-08 23:46:14 +0100

    Chapter 6

--- hittable.h
+++ hittable.h
@@ -1,6 +1,8 @@
 #ifndef HITTABLEH
 #define HITTABLEH
 
+#include "aabb.h"
+
 class material;
 
 class hit_record {
@@ -9,6 +11,8 @@ class hit_record {
         vec3 normal;
         shared_ptr<material> mat;
         double t;
+        double u;
+        double v;
         bool front_face;
 
         void set_face_normal(const ray& r, const vec3& outward_normal) {
@@ -26,6 +30,9 @@ class hittable {
         virtual ~hittable() = default;
 
         virtual bool hit(const ray& r, interval ray_t, hit_record& rec) const = 0;
+
+        virtual aabb bounding_box() const = 0;
 };
+
 
 #endif
--- sphere.h
+++ sphere.h
@@ -7,11 +7,19 @@ class sphere: public hittable {
     public:
         // Static Sphere
         sphere(const point3& static_center, double radius, shared_ptr<material> mat) 
-            : center(static_center, vec3(0,0,0)), radius(std::fmax(0,radius)), mat(mat) {}
+            : center(static_center, vec3(0,0,0)), radius(std::fmax(0,radius)), mat(mat) {
+                auto rvec = vec3(radius, radius, radius);
+                bbox = aabb(static_center - rvec, static_center + rvec);
+            }
 
         // Moving Sphere
         sphere(const point3& center1, const point3& center2, double radius, shared_ptr<material> mat)
-            : center(center1, center2 - center1), radius(std::fmax(0, radius)), mat(mat) {}
+            : center(center1, center2 - center1), radius(std::fmax(0, radius)), mat(mat) {
+                auto rvec = vec3(radius, radius, radius);
+                aabb box1(center.at(0) - rvec, center.at(0) + rvec);
+                aabb box2(center.at(1) - rvec, center.at(1) + rvec);
+                bbox = aabb(box1, box2);
+            }
 
         bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
             point3 current_center = center.at(r.time());
@@ -39,16 +47,27 @@ class sphere: public hittable {
             rec.p = r.at(rec.t);
             vec3 outward_normal = (rec.p - current_center) / radius;
             rec.set_face_normal(r, outward_normal);
+            get_sphere_uv(outward_normal, rec.u, rec.v);
             rec.mat = mat;
 
             return true;
         }
 
+        aabb bounding_box() const override { return bbox; }
+
     private:
         ray center;
         double radius;
         shared_ptr<material> mat;
+        aabb bbox;
 
+        static void get_sphere_uv(const point3& p, double& u, double& v) {
+            auto theta = std::acos(-p.y());
+            auto phi = std::atan2(-p.z(), p.x()) + pi;
+
+            u = phi / (2*pi);
+            v = theta / pi;
+        }
 };
 
 #endif
